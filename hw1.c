f95875672031eace6e6b131754d6f84c04b198b6
richard.z <rlziii@icloud.com>
2018-02-05 21:32:00 -0500

    Structs, and functions, and pointers, oh my

--- hw1.c
+++ hw1.c
@@ -8,30 +8,37 @@
 #define SJF 1;
 #define RR 2;
 
-int main(void)
+// Holds information for a single process
+typedef struct Process {
+    char *processName;
+    int arrivalTime;
+    int burstTime;
+} Process;
+
+// Holds information for a
+typedef struct Timeline {
+    int processCount;
+    int runTime;
+    int schedulingAlgorithm;
+    int timeQuantum;
+
+    Process **processes;
+} Timeline;
+
+void readInputFile(Timeline *inputTimeline)
 {
-    int processCount = 0;
-    int runTime = 0;
-    int schedulingAlgorithm = FCFS;
-    int timeQuantum = 0;
-
-    char **processNames;
-    int *arrivalTimes;
-    int *burstTimes;
-
     char line[256];
     char tempToken[256];
     char tempValue[256];
 
     int headerComplete = 0;
-
     int i;
 
     FILE *inputFile = fopen(DEFAULT_INPUT_FILE, "r");
 
     if (inputFile == NULL) {
         printf("Input file could not be opened.");
-        return 1;
+        return;
     }
 
     while (fgets(line, sizeof(line), inputFile)) {
@@ -44,21 +51,21 @@ int main(void)
         sscanf(line, "%s %s", tempToken, tempValue);
 
         if (strcmp(tempToken, "processcount") == 0) {
-            processCount = atoi(tempValue);
+            inputTimeline->processCount = atoi(tempValue);
         } else if (strcmp(tempToken, "runfor") == 0) {
-            runTime = atoi(tempValue);
+            inputTimeline->runTime = atoi(tempValue);
         } else if (strcmp(tempToken, "use") == 0) {
             if (strcmp(tempValue, "fcfs") == 0) {
-                schedulingAlgorithm = FCFS;
+                inputTimeline->schedulingAlgorithm = FCFS;
                 headerComplete = 1;
             } else if (strcmp(tempValue, "sjf") == 0) {
-                schedulingAlgorithm = SJF;
+                inputTimeline->schedulingAlgorithm = SJF;
                 headerComplete = 1;
             } else if (strcmp(tempValue, "rr") == 0) {
-                schedulingAlgorithm = RR;
+                inputTimeline->schedulingAlgorithm = RR;
             }
         } else if (strcmp(tempToken, "quantum") == 0) {
-            timeQuantum = atoi(tempValue);
+            inputTimeline->timeQuantum = atoi(tempValue);
             headerComplete = 1;
         }
 
@@ -67,38 +74,52 @@ int main(void)
         }
     }
 
-    processNames = malloc(sizeof(char *) * processCount);
+    // Make space for the processes
+    inputTimeline->processes = malloc(sizeof(Process *) * inputTimeline->processCount);
 
-    for (i = 0; i < processCount; i++) {
-        processNames[i] = malloc(sizeof(char *) * 256);
+    // Make space for the process names
+    for (i = 0; i < inputTimeline->processCount; i++) {
+        inputTimeline->processes[i] = malloc(sizeof(Process));
+        inputTimeline->processes[i]->processName = malloc(sizeof(char *) * 256);
     }
-
-    arrivalTimes = malloc(sizeof(int) * processCount);
-    burstTimes = malloc(sizeof(int) * processCount);
 
     // EXAMPLE: process name P1 arrival 3 burst 5
     //          process name P2 arrival 0 burst 9
-    for (i = 0; i < processCount; i++) {
+    for (i = 0; i < inputTimeline->processCount; i++) {
         fgets(line, sizeof(line), inputFile);
 
         if (strcmp(line, "end") == 0) {
             break;
         }
 
-        sscanf(line, "%*s %*s %s %*s %d %*s %d", processNames[i], &arrivalTimes[i], &burstTimes[i]);
+        sscanf(line, "%*s %*s %s %*s %d %*s %d", inputTimeline->processes[i]->processName, &inputTimeline->processes[i]->arrivalTime, &inputTimeline->processes[i]->burstTime);
     }
 
     printf("\n\n");
-    printf("Process Count        : %d\n", processCount);
-    printf("Run Time             : %d\n", runTime);
-    printf("Scheduling Algorithm : %d\n", schedulingAlgorithm);
-    printf("Time Quantum         : %d\n", timeQuantum);
-
-    for (i = 0; i < processCount; i++) {
-        printf("%s %d %d\n", processNames[i], arrivalTimes[i], burstTimes[i]);
-    }
+    printf("Process Count        : %d\n", inputTimeline->processCount);
+    printf("Run Time             : %d\n", inputTimeline->runTime);
+    printf("Scheduling Algorithm : %d\n", inputTimeline->schedulingAlgorithm);
+    printf("Time Quantum         : %d\n", inputTimeline->timeQuantum);
 
     fclose(inputFile);
+}
+
+int main(void)
+{
+    Timeline *inputTimeline = malloc(sizeof(Timeline));
+
+    int i;
+
+    readInputFile(inputTimeline);
+
+    for (i = 0; i < inputTimeline->processCount; i++) {
+        printf("%s %d %d\n", inputTimeline->processes[i]->processName, inputTimeline->processes[i]->arrivalTime, inputTimeline->processes[i]->burstTime);
+    }
+
+    for (i = 0; i < inputTimeline->processCount; i++) {
+        // free(inputTimeline->processes[i]->processName);
+    }
+    // free(inputTimeline->processes);
 
     return 0;
 }
